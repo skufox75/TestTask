60d1c1124e69da7d7a277bc9dbe775752162d24e
User <User@SKYFOX75>
2024-12-29 19:13:34 +0300

    Add project files.

---
+++ TestTask/Program.cs
@@ -0,0 +1,38 @@
+using Microsoft.EntityFrameworkCore;
+using TestTask.Data;
+using TestTask.Models;
+using TestTask.Services.Implementations;
+using TestTask.Services.Interfaces;
+
+namespace TestTask
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+            builder.Services.AddScoped<IBookService, BookService>();
+            builder.Services.AddDbContext<ApplicationDbContext>(options =>
+                options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+            builder.Services.AddControllers();
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+
+            var app = builder.Build();
+
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseAuthorization();
+
+            app.MapControllers();
+
+            app.Run();
+        }
+    }
+}
---
+++ TestTask/Services/Implementations/BookService.cs
@@ -0,0 +1,31 @@
+﻿using System.Linq;
+using TestTask.Data;
+using TestTask.Models;
+using TestTask.Services.Interfaces;
+
+namespace TestTask.Services.Implementations
+{
+    public class BookService : IBookService
+    {
+        private readonly ApplicationDbContext _context;
+        public BookService(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        public Task<Book> GetBook()
+        {
+            Book book = _context.Books.
+                OrderByDescending(b => b.Price * b.QuantityPublished).FirstOrDefault();
+            return Task.FromResult(book);
+        }
+        //public Task<Book> GetBook()
+        //{
+        //    throw new NotImplementedException();
+        //}
+
+        public Task<List<Book>> GetBooks()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
